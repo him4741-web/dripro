911400cd6221340a170aa3c29ae23d33ea889747
him4741-web <him4741@gmail.com>
2026-03-08 06:45:38 +0000

    Update sw.js with real Firebase config for FCM push notifications

--- public/sw.js
+++ public/sw.js
@@ -2,12 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: self.FIREBASE_API_KEY || "YOUR_API_KEY",
-  authDomain: self.FIREBASE_AUTH_DOMAIN || "YOUR_AUTH_DOMAIN",
-  projectId: self.FIREBASE_PROJECT_ID || "YOUR_PROJECT_ID",
-  storageBucket: self.FIREBASE_STORAGE_BUCKET || "YOUR_STORAGE_BUCKET",
-  messagingSenderId: self.FIREBASE_MESSAGING_SENDER_ID || "YOUR_SENDER_ID",
-  appId: self.FIREBASE_APP_ID || "YOUR_APP_ID",
+  apiKey: "AIzaSyA6IxElYpr3H3i4to38XxYPK6njapXxZ84",
+  authDomain: "dripro-bfc0f.firebaseapp.com",
+  projectId: "dripro-bfc0f",
+  storageBucket: "dripro-bfc0f.firebasestorage.app",
+  messagingSenderId: "510245379594",
+  appId: "1:510245379594:web:cf6e18de231e21daae89f3",
 });
 
 const messaging = firebase.messaging();
